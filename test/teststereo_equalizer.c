4314141e96c9ecc1d1d219b0ac1b01705b1c4047
Erik Hofman <erik@ehofman.com>
2020-06-23 10:34:03 +0200

    Updated parameters

--- test/teststereo_equalizer.c
+++ test/teststereo_equalizer.c
@@ -5,23 +5,23 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *    1. Redistributions of source code must retain the above copyright notice,
  *        this list of conditions and the following disclaimer.
- * 
+ *
  *    2. Redistributions in binary form must reproduce the above copyright
  *        notice, this list of conditions and the following disclaimer in the
  *        documentation and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY ADALIN B.V. ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
  * NO EVENT SHALL ADALIN B.V. OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUTOF THE USE 
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUTOF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * The views and conclusions contained in the software and documentation are
@@ -117,6 +117,8 @@ int main(int argc, char **argv)
             testForState(res, "aaxMixerStart");
 
             /* equalizer */
+            printf("%2.1f | %5.0f Hz | %2.1f | %5.0f Hz | %2.1f | %5.0f Hz | %2.1f\n", 1.0f, 500.0f, 0.1f, 2000.0f, 0.5f, 8000.0f, 0.1f);
+
             filter = aaxFilterCreate(config, AAX_EQUALIZER);
             testForError(filter, "aaxFilterCreate");
 
@@ -125,7 +127,11 @@ int main(int argc, char **argv)
             testForState(res, "aaxFilterSetSlot/0");
 
             res = aaxFilterSetSlot(filter, 1, AAX_LINEAR,
-                                              8000.0f, 0.1f, 0.5f, 2.0f);
+                                              2000.0f, 0.1f, 0.5f, 2.0f);
+            testForState(res, "aaxFilterSetSlot/1");
+
+            res = aaxFilterSetSlot(filter, 2, AAX_LINEAR,
+                                              8000.0f, 0.5f, 0.1f, 2.0f);
             testForState(res, "aaxFilterSetSlot/1");
 
             res = aaxFilterSetState(filter, AAX_TRUE);
