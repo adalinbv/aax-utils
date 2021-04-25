1a6b57b277dd3e5fa95adfc305a4ce9fe80372f0
Erik <erik@ehofman.com>
2021-04-25 23:05:41 +0200

    Setting the locale messes up playback

--- src/aaxplaymidi.cpp
+++ src/aaxplaymidi.cpp
@@ -184,7 +184,8 @@ void play(char *devname, enum aaxRenderMode mode, char *infile, char *outfile,
 int verbose = 0;
 int main(int argc, char **argv)
 {
-    std::setlocale(LC_ALL, "");
+  // setting the locale messes up playback
+//  std::setlocale(LC_ALL, "");
 //  std::locale::global(std::locale(""));
 //  std::cout.imbue(std::locale());
 
