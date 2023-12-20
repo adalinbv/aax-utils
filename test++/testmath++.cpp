4d2e57544a94b4c53a3295b9e05ff8e61cd64efe
Erik Hofman <erik@ehofman.com>
2023-12-20 11:24:59 +0100

    Matrix is deprecated, use Matrix64

--- test++/testmath++.cpp
+++ test++/testmath++.cpp
@@ -76,7 +76,6 @@ int main(int argc, char **argv)
     v3 = 3.1415926536;
     printf("set Vector(%2.1f, %2.1f, %2.1f) = 3.1415926535: Vector(%5.4f, %5.4f, %5.4f)\n", v2[0], v2[1], v2[2], v3[0], v3[1], v3[2]);
 
-    aax::Matrix im32, m32;
     aax::Matrix64 im, m1, m2;
 
     LOG("Initializing",m1,m2,true);
@@ -84,28 +83,9 @@ int main(int argc, char **argv)
     m1.rotate(0.13, 1.0, 0.0, 0.0);
     LOG("Rotating",m1,m2,false);
 
-    m32.rotate(0.13f, 1.0f, 0.0f, 0.0f);
-    m2 = m32;
-    LOG("Convert from 32-bit to 64-bit",m1,m2,true);
-
-    m1.rotate(-0.26, 1.0, 0.0f, 0.0f);
-    m32.rotate(-0.26f, 1.0f, 0.0, 0.0);
-    aax::Matrix64 m3 = m32;
-    LOG("Rotating M1 and M32",m1,m3,true);
-
     m1 += pos;
-    m3 = m1 * im;
+    aax::Matrix64 m3 = m1 * im;
     LOG("Multiplying identity matrix by m1",m3,m1,true);
-
-    m1.set(pos, at, up); m1.rotate(0.13f, 1.0f, 0.0f, 0.0f);
-    m32.set(pos32, at, up); m32.rotate(0.13, 1.0, 0.0, 0.0);
-    m3 = m32;
-    LOG("Testing orientation",m1,m3,true);
-
-    m2 = ~m1;
-    m32.inverse();
-    m3 = m32;
-    LOG("Inverse matrix",m2,m3,true);
 
     std::cout << std::endl << "Matrix mtx and mtx64:\n" << (m3);
 
@@ -113,10 +93,6 @@ int main(int argc, char **argv)
     aaxVec3f _at, _up;
     m3.get(_pos, _at, _up);
     printf("mtx:   at(%2.1f, %2.1f, %2.1f), up(%2.1f, %2.1f, %2.1f), pos(%2.1f, %2.1f, %2.1f)\n", _at[0], _at[1], _at[2], _up[0], _up[1], _up[2], _pos[0], _pos[1], _pos[2]);
-
-    aaxVec3f _pos32, _at32, _up32;
-    m32.get(_pos32, _at32, _up32);
-    printf("mtx64: at(%2.1f, %2.1f, %2.1f), up(%2.1f, %2.1f, %2.1f), pos(%2.1f, %2.1f, %2.1f)\n", _at32[0], _at32[1], _at32[2], _up32[0], _up32[1], _up32[2], _pos32[0], _pos32[1], _pos32[2]);
 
     return 0;
 }
